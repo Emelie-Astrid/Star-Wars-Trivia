091ced21baff8cc597d14f1b14b8d16a8e750255
Emelie <Emelie.almlof@gmail.com>
2023-03-09 10:04:03 +0900

    name lists

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+//Create a class that you name Character with 
+//the properties for name, gender, height, mass, 
+//hairColor, height, skinColor, eyeColor, movies 
+//and pictureUrl.
 class Character {
     constructor(name, gender, height, mass, hairColor, skinColor, eyeColor, movies, pictureUrl){
         this.name = name;
@@ -12,6 +16,43 @@ class Character {
     }
 }
 
-let newCharacter = new Character ("Kylo Ren", "Male", "189 cm", "80 kg", "black", "pale", "brown", "3", "link");
+// let newCharacter = new Character ("Kylo Ren", "Male", "189 cm", "80 kg", "black", "pale", "brown", "3", "link");
 
-console.log(newCharacter);
+// console.log(newCharacter);
+
+//The user should be able to choose two characters (Character 1 & 2) 
+//using each list. The lists must consist of at least six names of 
+//characters from the Star Wars universe found in the API.
+
+let nameListOne = document.querySelector("#name-list-1");
+let nameListTwo = document.querySelector("#name-list-2");
+
+async function getNames() {
+    let data = await fetch("https://swapi.dev/api/people/?format=json");
+    let json = await data.json();
+    return json;
+}
+
+// let showNames = (obj) => {
+//     let listName = document.createElement("li");
+//     listName.innerText = `${obj.results.name}`;
+//     nameListOne.append(listName);
+//     return li;
+// }
+
+getNames().then((data) => {
+    data.results.forEach((person)=> {
+        let listName = document.createElement("li");
+        listName.innerText = person.name;
+        nameListOne.append(listName);
+    });
+});
+
+getNames().then((data) => {
+    data.results.forEach((person)=> {
+        let listName = document.createElement("li");
+        listName.innerText = person.name;
+        nameListTwo.append(listName);
+    });
+});
+
