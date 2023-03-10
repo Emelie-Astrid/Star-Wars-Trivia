aa875354c3c31237b574b3ed002c16ad765f9b79
Emelie <Emelie.almlof@gmail.com>
2023-03-10 13:18:08 +0900

    img and name works for list one

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
-//Create a class that you name Character with 
-//the properties for name, gender, height, mass, 
-//hairColor, height, skinColor, eyeColor, movies 
-//and pictureUrl.
+let nameListOne = document.querySelector("#name-list-1");
+let nameListTwo = document.querySelector("#name-list-2");
+let charName = document.querySelector("#char1-name");
+let charName2 = document.querySelector("#char2-name");
+let infoButtonOne = document.querySelector("#char1-button");
+let infoButtonTwo = document.querySelector("#char2-button");
+let charImg = document.querySelector("#char1-image");
+let charImg2 = document.querySelector("#char2-image");
+let compareButton = document.querySelector("#compare-button");
+
 class Character {
     constructor(name, gender, height, mass, hairColor, skinColor, eyeColor, movies, pictureUrl){
         this.name = name;
         this.gender = gender;
         this.height = Number(height);
-        this.height = Number(mass);
+        this.mass = Number(mass);
         this.hairColor = hairColor;
         this.skinColor = skinColor;
         this.eyeColor = eyeColor;
@@ -16,40 +22,14 @@ class Character {
     }
 }
 
-// let newCharacter = new Character ("Kylo Ren", "Male", "189 cm", "80 kg", "black", "pale", "brown", "3", "link");
-// console.log(newCharacter);
-
-//The user should be able to choose two characters (Character 1 & 2) 
-//using each list. The lists must consist of at least six names of 
-//characters from the Star Wars universe found in the API.
-
-let nameListOne = document.querySelector("#name-list-1");
-let nameListTwo = document.querySelector("#name-list-2");
-// let listDivOne = document.querySelector("#first-list");
-// let listDivTwo = document.querySelector("#second-list");
-// let infoListOne = document.querySelector("#char-info-1");
-// let infoListTwo = document.querySelector("#char-info-2");
-let charName = document.querySelector("#char1-name");
-let charName2 = document.querySelector("#char2-name");
-let infoButtonOne = document.querySelector("#char1-button");
-let infoButtonTwo = document.querySelector("#char2-button");
-let compareButton = document.querySelector("#compare-button");
-
 async function getNames() {
     let data = await fetch("https://swapi.dev/api/people/?format=json");
     let json = await data.json();
-    console.log(json);
+    // console.log(json);
     return json;
 }
 
-// let showNames = (obj) => {
-//     let listName = document.createElement("li");
-//     listName.innerText = `${obj.results.name}`;
-//     nameListOne.append(listName);
-//     return li;
-// }
-
-//List 1
+//List 1 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 getNames().then((data) => {
     data.results.forEach((person)=> {
         let listName = document.createElement("li");
@@ -63,7 +43,7 @@ getNames().then((data) => {
     });
 });
 
-//List 2
+//List 2 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 getNames().then((data) => {
     data.results.forEach((person)=> {
         let listName = document.createElement("li");
@@ -76,36 +56,45 @@ getNames().then((data) => {
     });
 });
 
-//Info buttons
-
-// infoButtonOne.addEventListener("click", async () => {
-//     let selectedRadio = document.querySelector('input[name="person"]:checked');
-// })
-
-// let characterName = selectedRadio.value;
-
-// let data = await fetch(`https://swapi.dev/api/people/?search=${characterName}`);
-// let json = await data.json();
-// let characterData = json.result[0];
-
-// let character = new Character(
-//     characterData.name,
-//     characterData.gender,
-//     characterData.height,
-//     characterData.mass,
-//     characterData.hair_color,
-//     characterData.skin_color,
-//     characterData.eye_color,
-//     characterData.films,
-//     ""
-// );
-
-// let listItem = document.createElement("li");
-// listItem.innerText = JSON.stringify(character);
-// listDivOne.appendChild(listItem);
+//Info button one - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 infoButtonOne.addEventListener("click", async () => {
     let selectedRadio = document.querySelector('input[name="person"]:checked');
+    if (!selectedRadio) {
+      return;
+    }
+  
+    let characterName = selectedRadio.value;
+  
+    let data = await fetch(`https://swapi.dev/api/people/?search=${characterName}`);
+    let json = await data.json();
+    let characterData = json.results[0];
+
+    // console.log(characterData);
+  
+    let character = new Character(
+      characterData.name,
+      characterData.gender,
+      characterData.height,
+      characterData.mass,
+      characterData.hair_color,
+      characterData.skin_color,
+      characterData.eye_color,
+      characterData.films,
+      `/assets/${characterData.name.toLowerCase().replace(/ /g, "-")}.jpg`
+    );
+
+    // console.log(character.pictureUrl);
+
+    charName.innerText = characterData.name;
+    charImg.src = character.pictureUrl;
+
+    // console.log(character);
+  });
+
+  //Info button two - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  infoButtonTwo.addEventListener("click", async () => {
+    let selectedRadio = document.querySelector('input[name="person2"]:checked');
     if (!selectedRadio) {
       return;
     }
@@ -127,20 +116,16 @@ infoButtonOne.addEventListener("click", async () => {
       characterData.skin_color,
       characterData.eye_color,
       characterData.films,
-      ""
+      `/assets/${characterData.name.toLowerCase().replace(/ /g, "-")}.jpg`
     );
 
-    charName.innerText = characterData.name;
+    console.log(character.pictureUrl);
+
+    charName2.innerText = characterData.name;
+    charImg2.src = character.pictureUrl;
 
     // console.log(character);
-  
-    // Object.entries(character).forEach(([key, value]) => {
-    //     let listItem = document.createElement("li");
-    //     listItem.innerText = `${key}: ${value}`;
-    //     infoListOne.appendChild(listItem);
-    //   });
   });
-
 //The user should then be able to click a button to retrieve data 
 //about the characters. Once the data is retrieved, 
 //create two instances of the Character class, 
